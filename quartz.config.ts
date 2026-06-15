fbcec54678236400008feb50a7841e16b028965b
metaarchetech <metaarteorg@gmail.com>
2026-06-15 16:31:58 +0800

    fix: 移除 Obsidian 範本(frontmatter 破壞 build)+ ignorePatterns 防呆

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "vistwindev.github.io",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", "**/Templates/**", "**/Templates", ".obsidian", "**/scripts/**", "**/logs/**"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
